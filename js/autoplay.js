60fb04a00b7a00c163076ea4a512de8f2b791b28
abraha7x <miten.mitsurugi@gmail.com>
2018-06-28 18:14:01 -0500

    randomInit

--- js/autoplay.js
+++ js/autoplay.js
@@ -1,6 +1,13 @@
 var board = new Array(3);
 var turn;
 
+var Cross_Sellected_x;
+var Cross_Sellected_y;
+
+var Ball_Sellected_x;
+var Ball_Sellected_y;
+
+
 function ClearCell(x, y){
   board[x][y] = 0;
   cell = document.getElementById("c" + x + y)
@@ -21,9 +28,13 @@ function PaintCell(x, y){
 
   if(turn = "ball"){
     board[x][y] = 1;
+    Ball_Sellected_x = x;
+    Ball_Sellected_y = y;
   }
   else{
     board[x][y] = 2;
+    Cross_Sellected_x = x;
+    Cross_Sellected_y = y;
   }
 
   if(turn == "ball") turn = "cross";
@@ -35,10 +46,17 @@ function autoplay(){
 
   for(i=0; i<3; i++)
     board[i] = new Array(3);
-  turn = "ball";
+  turn = "cross";
+
+  Cross_Sellected_x = 4;
+  Cross_Sellected_y = 4;
+
+  Ball_Sellected_x = 4;
+  Ball_Sellected_y = 4;
 
   ClearBoard();
-  PaintCell(1,2);
+  SearchMove();
+
 
 }
 
